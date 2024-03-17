84172db0ac75896e3b01697b907f12c5a6f5093b
LinkCatList <vigalok@bk.ru>
2024-03-17 18:34:07 +0300

    now you should input string not vector of strings

--- src/lz78.cpp
+++ src/lz78.cpp
@@ -4,22 +4,33 @@
 template <typename T>
 std::ostream &operator <<(std::ostream &stream, const std::vector<T> &v){
     for (const auto &i : v){
-        stream << i << " ";
+        stream << i;
     }
     return stream;
 }
 
 int main() {
 
-    std::vector<std::string> s = {"22", "1", "a", "c", "a", "b", "a"};
+    std::string input;
+    std::cin >> input;
+
+    std::vector<std::string> s;
+
+    for (int i = 0; i < input.size(); i++) {
+        std::string tmp = {input[i]};
+        s.push_back(tmp);
+    }
 
     auto encode = lz78_Encode(s);
 
+    std::cout << "ENCODE:\n";
     for (auto &i: encode) {
         std::cout << std::get<0>(i) << " " << std::get<1>(i) << "\n"; 
     }
 
     std::vector<std::string> decode = lz78_Decode(encode);
+
+    std::cout << "DECODE:\n";
     std::cout << decode << "\n";
 
     return 0;
